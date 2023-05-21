5e9bf874b5a6709377f3ff30af392555d2f897b0
san <sanmathew2002@gmail.com>
2023-05-21 15:31:57 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import { useState } from "react";
+import { useState } from "react"
+function Header()
+{
 
-
-function Mybutton() {
-  const [count, setCount] = useState(0)
-  function press() {
-    setCount(count + 1)
-  }
-
-  return (
-    <button onClick={press} style={{height:100,width:100}}>the button was click {count} times .</button>
-
+  return(
+    <div className="header" style={{fontSize: 100, textAlign: "center"}}>
+      TODO list 
+    </div>
   )
 }
+function Footer()
+{
+
+  return(
+    <div className="header" style={{fontSize: 100, textAlign: "center",bottom:0}}>
+      Footer
+    </div>
+  )
+}
+
+function Todo({value})
+{
+  return(
+    <div className="todocomp" style={{width:"100%",height:20}}>{value}<button >Done</button><button>Delete</button></div>
+  )
+}
+function Todos()
+{
+  const [todo,setTodo]=useState()
+  return(
+    
+    <div className="header" style={{/* width:"100%" , */height:100,backgroundColor:"red",display:"flex",alignItems:"center",justifyContent:"center"}}>
+    <Todo value="market"/> 
+    <Todo value="market"/> 
+    <Todo value="market"/> 
+    <Todo value="market"/> 
+    <Todo value="market"/>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <>
-      <Mybutton />
-      <Mybutton />
+      <Header/>
+      <Todos/>
+      <Footer/>
     </>
   )
 }
